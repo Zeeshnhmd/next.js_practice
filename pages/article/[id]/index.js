b3c3287fb44fbf5815ae6e8752a71ae80df7e4f5
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-05-08 21:20:40 +0530

    fetching data using getServerSideProps and (getStaticProps and getStaticPaths)

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,15 +1,60 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-const Article = () => {
-	const router = useRouter();
-	const { id } = router.query;
+const Article = ({ articles }) => {
+	// const router = useRouter();
+	// const { id } = router.query;
 	return (
 		<div>
-			<h1> this is article {id}</h1>
+			<h1> {articles.title}</h1>
+			<p>{articles.body}</p> <br />
 			<Link href="/">Go Back</Link>
 		</div>
 	);
+};
+
+// this is how we can fetch data using getServerSideProps.
+
+// export const getServerSideProps = async (context) => {
+// 	const res = await fetch(
+// 		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+// 	);
+// 	const article = await res.json();
+// 	return {
+// 		props: {
+// 			article,
+// 		},
+// 	};
+// };
+
+// this is how we can fetch data using [getStaticProps ()] and [getStaticPaths ()]
+
+export const getStaticProps = async (context) => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+	);
+	const articles = await res.json();
+	return {
+		props: {
+			articles,
+		},
+	};
+};
+
+export const getStaticPaths = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+	const articles = await res.json();
+
+	// getting ids
+	const ids = articles.map((article) => article.id);
+
+	// getting paths
+	const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+	return {
+		paths,
+		fallback: true, // this means that is we going to path which does not exist in the data then it will show us a 404 page
+	};
 };
 
 export default Article;
